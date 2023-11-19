5e17aa714deef14a30ee8fb33429ba9abbec4045
utkaarsh <utkarshranpise29@gmail.com>
2023-11-20 00:35:01 +0530

    updated Dashboard UI

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -13,58 +13,58 @@ const Dashboard = () => {
                 </h1>
 
         <div className="row">
-            <div className="col-6 p-4">
-            <div className="card bg-success p-2 text-dark bg-opacity-10">
+            <div className="col-4 p-4 d-flex justify-content-center">
+            <div className="card bg-success p-2 text-dark bg-opacity-10" role='button' onClick={() => {
+                                    navigate('/game');
+                                }} >
                             <div className="card-body ">
-                                <h5 className="card-title level-title" >Level 1</h5>
-                                <p>Difficulty level : Easy</p>
+                                <h5 className="card-title level-title " >Level 1</h5>
+                                {/* <p>Difficulty level : Easy</p> */}
                                 <p>Grid size : 10 X 15</p>
                                 {/* <p className="card-text">With supporting text below as a natural lead-in to additional content.</p> */}
-                                <a href="#" className="btn btns" onClick={() => {
-                                    navigate('/game');
-                                }}>Go to game</a>
+                               
                             </div>
                         </div>
             </div>
-            <div className="col-6 p-4"> 
-            <div className="card bg-success p-2 text-dark bg-opacity-10">
-                            <div className="card-body ">
-                                <h5 className="card-title level-title" >Level 2</h5>
-                                <p>Difficulty level : Hard</p>
-                                <p>Grid size : 18 X 25</p>
-                                {/* <p className="card-text">With supporting text below as a natural lead-in to additional content.</p> */}
-                                <a href="#" className="btn btns" onClick={() => {
+            <div className="col-4 p-4 d-flex justify-content-center"> 
+            <div className="card bg-success p-2 text-dark bg-opacity-10" role='button' onClick={() => {
                                     navigate('/game2');
-                                }}>Go to game</a>
+                                }}>
+                            <div className="card-body ">
+                                <h5 className="card-title level-title"   >Level 2</h5>
+                                {/* <p>Difficulty level : Hard</p> */}
+                                <p>Grid size : 18 X 25</p> 
+                                {/* <p className="card-text">With supporting text below as a natural lead-in to additional content.</p> */}
+                                
                             </div>
                         </div>
             </div>
         </div>
 
 <div className="row">
-    <div className="col-6 p-4">
-    <div className="card bg-success p-2 text-dark bg-opacity-10">
+    <div className="col-4 p-4 d-flex justify-content-center">
+    <div className="card bg-success p-2 text-dark bg-opacity-10" role='button' onClick={() => {
+                                    navigate('/timerGame1');
+                                }}>
                     <div className="card-body ">
-                        <h5 className="card-title level-title" >Timer Level 1 </h5>
-                        <p>Difficulty level : Easy</p>
+                        <h5 className="card-title level-title"   >Timeout Level 1 </h5>
+                        {/* <p>Difficulty level : Easy</p> */}
                         <p>Grid size : 10 X 15</p>
                         {/* <p className="card-text">With supporting text below as a natural lead-in to additional content.</p> */}
-                        <a href="#" className="btn btns" onClick={() => {
-                            navigate('/timerGame1');
-                        }}>Go to game</a>
+                        
                     </div>
                 </div>
     </div>
-    <div className="col-6 p-4"> 
-    <div className="card bg-success p-2 text-dark bg-opacity-10">
+    <div className="col-4 p-4 d-flex justify-content-center"> 
+    <div className="card bg-success p-2 text-dark bg-opacity-10" role='button' onClick={() => {
+                                    navigate('/timerGame2');
+                                }}>
                     <div className="card-body ">
-                        <h5 className="card-title level-title" >Timer Level 2</h5>
-                        <p>Difficulty level : Hard</p>
+                        <h5 className="card-title level-title"   >Timeout Level 2</h5>
+                        {/* <p>Difficulty level : Hard</p> */}
                         <p>Grid size : 18 X 25</p>
                         {/* <p className="card-text">With supporting text below as a natural lead-in to additional content.</p> */}
-                        <a href="#" className="btn btns" onClick={() => {
-                            navigate('/timerGame2');
-                        }}>Go to game </a>
+                        
                     </div>
                 </div>
     </div>
